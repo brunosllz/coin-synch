14752ed20751b721e15455cc6effe1408a2631f3
brunosllz <brunoslvrlz13@gmail.com>
2023-07-22 18:07:12 -0300

    styles: added about us section styles

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -3,6 +3,10 @@ import { SectionContainer } from '@/components/SectionContainer'
 import logo from '@/assets/logo.svg'
 import hero1 from '@/assets/hero.png'
 import hero2 from '@/assets/hero-2.png'
+import bitcoin from '@/assets/bitcoin.svg'
+import ethereum from '@/assets/business-chart.svg'
+import businessChart from '@/assets/ethereum.svg'
+import laptopMobile from '@/assets/laptop-mobile.svg'
 import Image from 'next/image'
 import { Button } from '@/components/ui/button'
 import { ArrowRight } from 'lucide-react'
@@ -11,6 +15,47 @@ const LABEL_CONTENT = [
   { id: '1', label: 'Cryptos' },
   { id: '2', label: 'NFTs' },
   { id: '3', label: 'Games' },
+]
+
+const CARD_CONTENT = [
+  {
+    top: [
+      {
+        id: '1',
+        label: 'For your company',
+        title: 'Crypto Solutions',
+        content:
+          'Lorem ipsum dolor sit amet, consectetur adipiscing elit ut aliquam, ',
+        imageUrl: bitcoin,
+      },
+      {
+        id: '2',
+        label: 'For your company',
+        title: 'Crypto Solutions',
+        content:
+          'Lorem ipsum dolor sit amet, consectetur adipiscing elit ut aliquam, ',
+        imageUrl: ethereum,
+      },
+    ],
+    bottom: [
+      {
+        id: '3',
+        label: 'For your company',
+        title: 'Crypto Solutions',
+        content:
+          'Lorem ipsum dolor sit amet, consectetur adipiscing elit ut aliquam, ',
+        imageUrl: businessChart,
+      },
+      {
+        id: '4',
+        label: 'For your company',
+        title: 'Crypto Solutions',
+        content:
+          'Lorem ipsum dolor sit amet, consectetur adipiscing elit ut aliquam, ',
+        imageUrl: laptopMobile,
+      },
+    ],
+  },
 ]
 
 export default function Home() {
@@ -62,34 +107,67 @@ export default function Home() {
       <div className="mt-[42px] h-[335.06px] w-full bg-hero-pattern bg-cover bg-no-repeat" />
 
       <SectionContainer>
-        <div>
-          {Array.from({ length: 4 }).map((_, index) => {
+        <div className="mb-[130px] mt-[120px] grid grid-cols-[696px_1fr] gap-8 bg-gradient-to-b from-white/0 to-[#F7F7F7]">
+          {CARD_CONTENT.map((item, index) => {
             return (
-              <div key={index}>
-                <strong>For your company</strong>
-                <h3>Crypto Solutions</h3>
-                <p>
-                  Lorem ipsum dolor sit amet, consectetur adipiscing elit ut
-                  aliquam,{' '}
-                </p>
-              </div>
+              <ul key={index} className="flex flex-col gap-8">
+                <div className="grid grid-cols-[280px_1fr] gap-8">
+                  {item.top.map((item) => {
+                    return (
+                      <li
+                        key={item.id}
+                        className="flex flex-col gap-4 rounded-md bg-white p-6 shadow-xl"
+                      >
+                        <Image src={item.imageUrl} alt="" />
+
+                        <div>
+                          <strong>{item.label}</strong>
+                          <h3>{item.title}</h3>
+                          <p>{item.content} </p>
+                        </div>
+                      </li>
+                    )
+                  })}
+                </div>
+
+                <div className="grid grid-cols-[1fr_280px] gap-8">
+                  {item.top.map((item) => {
+                    return (
+                      <li
+                        key={item.id}
+                        className="flex flex-col gap-4 rounded-md bg-white p-6 shadow-xl"
+                      >
+                        <Image src={item.imageUrl} alt="" />
+
+                        <div>
+                          <strong>{item.label}</strong>
+                          <h3>{item.title}</h3>
+                          <p>{item.content} </p>
+                        </div>
+                      </li>
+                    )
+                  })}
+                </div>
+              </ul>
             )
           })}
-        </div>
-        <div>
-          <div>
-            <strong>Lorem ipsum </strong>
-            <h3>Lorem ipsum</h3>
-            <p>
-              Lorem ipsum dolor sit amet, consectetur adipiscing elit ut
-              aliquam, purus sit amet luctus venenatis, lectus magna fringilla
-              urna, porttitor
-            </p>
-          </div>
 
-          <button>Sign up now</button>
+          <div className="flex max-w-[406px] flex-col justify-center gap-10">
+            <div>
+              <h5 className="font-bold text-primary-500">Lorem ipsum </h5>
+              <h2 className="mb-4 mt-1 font-bold">Lorem ipsum</h2>
+              <p>
+                Lorem ipsum dolor sit amet, consectetur adipiscing elit ut
+                aliquam, purus sit amet luctus venenatis, lectus magna fringilla
+                urna, porttitor
+              </p>
+            </div>
+
+            <Button>Sign up now</Button>
+          </div>
         </div>
       </SectionContainer>
+
       <SectionContainer>
         <div>
           <div>
