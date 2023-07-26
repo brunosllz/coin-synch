3f85dba3bb1c3a4d358d5440e6aed89ded87a42e
brunosllz <brunoslvrlz13@gmail.com>
2023-07-26 14:32:59 -0300

    feat: added tooltip at sidebar links

--- src/app/dashboard/sidebar.tsx
+++ src/app/dashboard/sidebar.tsx
@@ -1,35 +1,62 @@
 import Image from 'next/image'
+import {
+  TooltipProvider,
+  Tooltip,
+  TooltipTrigger,
+  TooltipContent,
+} from '@/components/ui/tooltip'
 
 import ethereum from '@/assets/ethereum.svg'
 import bitcoin from '@/assets/bitcoin.svg'
 import businessChart from '@/assets/business-chart.svg'
 import wallet from '@/assets/wallet.svg'
 
+const NAV_LINKS = [
+  {
+    id: '1',
+    icon: wallet,
+    tooltipLabel: 'Lorem Ipsum',
+  },
+  {
+    id: '2',
+    icon: ethereum,
+    tooltipLabel: 'Lorem Ipsum',
+  },
+  {
+    id: '3',
+    icon: bitcoin,
+    tooltipLabel: 'Lorem Ipsum',
+  },
+  {
+    id: '4',
+    icon: businessChart,
+    tooltipLabel: 'Lorem Ipsum',
+  },
+]
+
 export function Sidebar() {
   return (
     <aside className="relative flex h-full flex-col border-y border-secondary-300 bg-white pb-16 pl-6 pr-[30px] pt-12">
       <nav>
         <ul className="flex flex-col gap-8">
-          <li>
-            <a>
-              <Image src={wallet} alt="" width={32} height={32} />
-            </a>
-          </li>
-          <li>
-            <a>
-              <Image src={ethereum} alt="" width={32} height={32} />
-            </a>
-          </li>
-          <li>
-            <a>
-              <Image src={bitcoin} alt="" width={32} height={32} />
-            </a>
-          </li>
-          <li>
-            <a>
-              <Image src={businessChart} alt="" width={32} height={32} />
-            </a>
-          </li>
+          {NAV_LINKS.map((link) => {
+            return (
+              <li key={link.id}>
+                <TooltipProvider delayDuration={300}>
+                  <Tooltip>
+                    <TooltipTrigger>
+                      <a>
+                        <Image src={link.icon} alt="" width={32} height={32} />
+                      </a>
+                    </TooltipTrigger>
+                    <TooltipContent side="right">
+                      <p>{link.tooltipLabel}</p>
+                    </TooltipContent>
+                  </Tooltip>
+                </TooltipProvider>
+              </li>
+            )
+          })}
         </ul>
       </nav>
     </aside>
--- src/components/ui/tooltip.tsx
+++ src/components/ui/tooltip.tsx
@@ -1,9 +1,9 @@
-"use client"
+'use client'
 
-import * as React from "react"
-import * as TooltipPrimitive from "@radix-ui/react-tooltip"
+import * as React from 'react'
+import * as TooltipPrimitive from '@radix-ui/react-tooltip'
 
-import { cn } from "@/lib/utils"
+import { cn } from '@/lib/utils'
 
 const TooltipProvider = TooltipPrimitive.Provider
 
@@ -11,19 +11,24 @@ const Tooltip = TooltipPrimitive.Root
 
 const TooltipTrigger = TooltipPrimitive.Trigger
 
+const TooltipArrow = TooltipPrimitive.Arrow
+
 const TooltipContent = React.forwardRef<
   React.ElementRef<typeof TooltipPrimitive.Content>,
   React.ComponentPropsWithoutRef<typeof TooltipPrimitive.Content>
->(({ className, sideOffset = 4, ...props }, ref) => (
+>(({ children, className, sideOffset = 4, ...props }, ref) => (
   <TooltipPrimitive.Content
     ref={ref}
     sideOffset={sideOffset}
     className={cn(
-      "z-50 overflow-hidden rounded-md border border-slate-200 bg-white px-3 py-1.5 text-sm text-slate-950 shadow-md animate-in fade-in-0 zoom-in-95 data-[state=closed]:animate-out data-[state=closed]:fade-out-0 data-[state=closed]:zoom-out-95 data-[side=bottom]:slide-in-from-top-2 data-[side=left]:slide-in-from-right-2 data-[side=right]:slide-in-from-left-2 data-[side=top]:slide-in-from-bottom-2 dark:border-slate-800 dark:bg-slate-950 dark:text-slate-50",
-      className
+      'z-50 overflow-hidden rounded-[4px] bg-primary-500 px-6 py-2 text-sm text-white shadow-md animate-in fade-in-0 zoom-in-95 data-[state=closed]:animate-out data-[state=closed]:fade-out-0 data-[state=closed]:zoom-out-95 data-[side=bottom]:slide-in-from-top-2 data-[side=left]:slide-in-from-right-2 data-[side=right]:slide-in-from-left-2 data-[side=top]:slide-in-from-bottom-2',
+      className,
     )}
     {...props}
-  />
+  >
+    <TooltipArrow className="fill-primary-500" />
+    {children}
+  </TooltipPrimitive.Content>
 ))
 TooltipContent.displayName = TooltipPrimitive.Content.displayName
 
